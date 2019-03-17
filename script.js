25c2f8d630357bba7ce91a818fddb224e684e22f
MuhammadAlaeldin <muhammadalaa27@gmail.com>
2019-03-17 03:05:05 +0200

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //var questions
 var allQuestions = [
-    ["What is JavaScript?", "Another name for Java", "A scripting language mostly for the web", "When you use Java without compiling", "B"],
-    ["For what JavaScript is used?", "frontend", "backend", "both", "C"],
-    ["What is closure in javaScript?", "A closure is an inner function that has access to the variables in the outer function’s scope chain", "A closure is an outer function that has access to the variables in the inner function’s scope chain", "something else", "A"],
-    ["What will be the output when the following code is executed? console.log(false == '0')", "false", "true", "NaN", "B"],
-    ["What will be the output when the following code is executed? console.log(false === '0')", "false", "true", "NaN", "A"],
-    ["What will be the output when the following code is executed? console.log((function f(n){return ((n > 1) ? n * f(n-1) : n)})(10));", "10", "10!", "90", "B"],
-    ["What do the following lines output?<br> console.log(3 > 2 > 1);", "true", "false", "", "B"],
-    ["How do you add an element at the begining of an array?", "arr[1] = 'element';", "arr.push('element');", "arr.unshift('element';", "C"],
-    ["How do you add an element at the end of an array?", "arr[5] = 'element';", "arr.push('element');", "arr.unshift('element';", "B"],
-    ["What is the type of 'NaN'? use console.log(type of NaN === \"__\"); to check it.", "string", "boolean", "number", "C"]
+    ["<h4>What is JavaScript?</h4>", "Another name for Java", "A scripting language mostly for the web", "When you use Java without compiling", "B"],
+    ["<h4>For what JavaScript is used?</h4>", "frontend", "backend", "both", "C"],
+    ["<h4>What is closure in javaScript?</h4>", "A closure is an inner function that has access to the variables in the outer function’s scope chain", "A closure is an outer function that has access to the variables in the inner function’s scope chain", "something else", "A"],
+    ["<h4>What will be the output when the following code is executed? <br> console.log(false == '0')</h4>", "false", "true", "NaN", "B"],
+    ["<h4>What will be the output when the following code is executed? <br> console.log(false === '0')</h4>", "false", "true", "NaN", "A"],
+    ["<h4>What will be the output when the following code is executed? <br> console.log((function f(n){return ((n > 1) ? n * f(n-1) : n)})(10));</h4>", "10", "10!", "90", "B"],
+    ["<h4>What do the following lines output?<br> console.log(3 > 2 > 1);</h4>", "true", "false", "", "B"],
+    ["<h4>How do you add an element at the begining of an array?</h4>", "arr[1] = 'element';", "arr.push('element');", "arr.unshift('element';", "C"],
+    ["<h4>How do you add an element at the end of an array?</h4>", "arr[5] = 'element';", "arr.push('element');", "arr.unshift('element';", "B"],
+    ["<h4>What is the type of 'NaN'? <br> use console.log(type of NaN === \"__\"); to check it.</h4>", "string", "boolean", "number", "C"]
 ];
 var fiveQuestions = [];
 for (var i = 0; i < 5; i++) {
@@ -37,14 +37,12 @@ function question() {
 question();
 
 var score = 0;
-
 function validateA() {
     if (correct == "A") {
         ++score;
         question();
     }
     else {
-        --score;
         question();
     }
 }
@@ -54,7 +52,6 @@ function validateB() {
         question();
     }
     else {
-        --score;
         question();
     }
 }
@@ -64,7 +61,6 @@ function validateC() {
         question();
     }
     else {
-        --score;
         question();
     }
 }
@@ -79,11 +75,11 @@ function validateC() {
 function finish() {
     var r = document.getElementById("score");
     // if (rest.length == 0) {
-    if (score < 3) {
-        r.innerHTML = "You need to know more";
+    if (score <= 0) {
+        r.innerHTML = "0";
     }
     else {
-        r.innerHTML = "Good Job";
+        r.innerHTML = score;
     }
     // } else {
     //     for (var i = 0; i < rest.length; i++) {
